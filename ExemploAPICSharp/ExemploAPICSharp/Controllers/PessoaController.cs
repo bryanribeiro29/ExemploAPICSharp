2b9b248bb3b170f341988b514910495eb5e76fbb
bryanribeiro29 <129908079+bryanribeiro29@users.noreply.github.com>
2024-02-02 22:16:20 -0300

    commit criando pessoa

---
+++ ExemploAPICSharp/ExemploAPICSharp/Controllers/PessoaController.cs
@@ -0,0 +1,23 @@
+﻿using ExemploAPICSharp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+//using ExemploAPICSharp.Model; clicando na onde está com erro e alt + enter
+
+namespace ExemploAPICSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet("GetPessoa")]
+        public Pessoa GetPessoa()
+        {
+            var vPessoa = new Pessoa();
+            vPessoa.Id = 1;
+            vPessoa.Nome = "José da Couves";
+            vPessoa.NumeroCelular = "(18) 98569-7845";
+            
+            return vPessoa;
+        }
+    }
+}
---
+++ ExemploAPICSharp/ExemploAPICSharp/Model/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿namespace ExemploAPICSharp.Model
+{
+    public class Pessoa
+    {
+        //prop e Tab = propriedade (exatamente a de baixo).
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NumeroCelular { get; set;}
+        public DateOnly DataNascimento { get; set; }
+    }
+    
+}
